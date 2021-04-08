3bd6b54f12178d7d941f17444e93128c0ba4b2b7
Std1o <37378410+Std1o@users.noreply.github.com>
2021-04-08 11:44:33 +0300

    proximity sensor

--- app/src/main/java/com/stdio/lightandproximitysensors/MainActivity.java
+++ app/src/main/java/com/stdio/lightandproximitysensors/MainActivity.java
@@ -12,18 +12,23 @@ import android.widget.TextView;
 public class MainActivity extends AppCompatActivity {
 
     TextView tvLight;
+    TextView tvProximity;
     SensorManager sensorManager;
     Sensor sensorLight;
+    Sensor sensorProximity;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         tvLight = findViewById(R.id.tvLight);
+        tvProximity = findViewById(R.id.tvProximity);
+
         sensorManager = (SensorManager) getSystemService(SENSOR_SERVICE);
         sensorLight = sensorManager.getDefaultSensor(Sensor.TYPE_LIGHT);
-        sensorManager.registerListener(listenerLight, sensorLight,
-                SensorManager.SENSOR_DELAY_NORMAL);
+        sensorProximity = sensorManager.getDefaultSensor(Sensor.TYPE_PROXIMITY);
+        sensorManager.registerListener(listenerLight, sensorLight, SensorManager.SENSOR_DELAY_NORMAL);
+        sensorManager.registerListener(listenerProximity, sensorProximity, SensorManager.SENSOR_DELAY_NORMAL);
     }
 
     SensorEventListener listenerLight = new SensorEventListener() {
@@ -34,7 +39,18 @@ public class MainActivity extends AppCompatActivity {
 
         @Override
         public void onSensorChanged(SensorEvent event) {
-            tvLight.setText("Light Sensor: " + event.values[0]);
+            tvLight.setText("Light Sensor: " + event.values[0] + " luxes");
+        }
+    };
+    SensorEventListener listenerProximity = new SensorEventListener() {
+
+        @Override
+        public void onAccuracyChanged(Sensor sensor, int accuracy) {
+        }
+
+        @Override
+        public void onSensorChanged(SensorEvent event) {
+            tvProximity.setText("Proximity Sensor: " + event.values[0] + " см");
         }
     };
 }
